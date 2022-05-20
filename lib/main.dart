223a7248b3928346d2d15bd0e96ab38b415e1656
AhmedElSayyadMohamed <elsayyada197@gmail.com>
2022-05-20 18:37:58 +0200

    event notifcation

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,8 @@ import 'package:club_cast/data_layer/dio/dio_setup.dart';
 import 'package:club_cast/presentation_layer/components/constant/constant.dart';
 import 'package:club_cast/presentation_layer/components/theme/app_theme.dart';
 import 'package:club_cast/presentation_layer/layout/layout_screen.dart';
-import 'package:club_cast/presentation_layer/screens/splash_onboarding/onboarding_screen.dart';
 import 'package:club_cast/presentation_layer/screens/user_screen/login_screen/login_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_background/flutter_background.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'data_layer/bloc/bloc_observer/bloc_observer.dart';
 import 'data_layer/bloc/intial_cubit/general_app_cubit.dart';
@@ -31,7 +29,6 @@ void main() async {
     isDark = CachHelper.getData(key: 'isDark');
   }
   Widget startApp;
-
   if (await CachHelper.getData(key: 'token') != null) {
     startApp = LayoutScreen();
   } else {
